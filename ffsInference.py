a05f236216cb95d58a6736f247960aaad165863f
battlejacket <erikgustafsson93@gmail.com>
2025-05-12 17:41:35 +0000

    fix inference batch size

--- ffsInference.py
+++ ffsInference.py
@@ -9,47 +9,21 @@ from upt.datasets.ffs_dataset import ffsDataset
 from upt.collators.ffs_collator import ffsCollator
 import copy
 
-original = [[1, 2], [3, 4]]
-deep_copy = copy.deepcopy(original)
-
-
-
 class ffsInference:
-    def __init__(self, base_dataset, numSupernodes,
-                  model = None, device = None):
-        
+    def __init__(self, base_dataset, numSupernodes, model=None, device=None):
         self.model = model
         self.base_dataset = base_dataset
-        # self.totalPoints = totalPoints
         self.numSupernodes = numSupernodes
         self.device = device
-        # self.xMin = base_dataset.crop_values[0][0]
-        # self.xMax = base_dataset.crop_values[1][0]
-
         self.inference_dataset = copy.deepcopy(base_dataset)
-
 
     def infer(self, parameter_sets, output_pos=None, batch_size=1):
         """
         Perform inference using the same DataLoader setup as training.
         """
-        # Create a temporary dataset for inference
-        # self.inference_dataset = ffsDataset(
-        #     root=self.base_dataset.root,
-        #     num_inputs=self.base_dataset.num_inputs,
-        #     num_outputs=self.base_dataset.num_outputs,
-        #     mode="inference",
-        #     crop_values=self.base_dataset.crop_values,
-        #     parameter_sets=parameter_sets,
-        #     useMesh=self.base_dataset.useMesh,
-        #     meshParameters=self.base_dataset.meshParameters,
-        #     customOutputPos=output_pos,
-        # )
-        
-        # self.inference_dataset = copy.deepcopy( self.base_dataset)
         self.inference_dataset.setInferenceMode(parameter_sets)
-        self.inference_dataset.customOutputPos=output_pos
-        
+        self.inference_dataset.customOutputPos = output_pos
+
         # Create a DataLoader for inference
         inference_dataloader = DataLoader(
             dataset=self.inference_dataset,
@@ -58,16 +32,10 @@ class ffsInference:
         )
 
         all_predictions = []
-        all_parameters = []
         all_points = []
 
         # Perform inference
         for batch in inference_dataloader:
-            # for key, value in batch.items():
-            #     if key == 'name':
-            #         continue
-            #     print(key, value.shape)
-
             with torch.no_grad():
                 y_hat = self.model(
                     input_feat=batch["input_feat"].to(self.device),
@@ -77,95 +45,90 @@ class ffsInference:
                     output_pos=batch["output_pos"].to(self.device),
                     re=batch["re"].to(self.device),
                 )
-            all_predictions.append(self.base_dataset.denormalize_feat(y_hat.cpu()))
-            # all_parameters.extend(batch["name"])
-            all_points.append(self.base_dataset.denormalize_pos(batch["output_pos"]))
+            all_predictions.append(y_hat.cpu())
+            all_points.append(batch["output_pos"])
 
+        # Concatenate predictions and points across batches
+        # all_predictions = torch.cat(all_predictions, dim=0)
+        # all_points = torch.cat(all_points, dim=0)
+        all_predictions.append(self.base_dataset.denormalize_feat(y_hat.cpu()))
+        all_points.append(self.base_dataset.denormalize_pos(batch["output_pos"]))
 
         return {
-            # "parameters": all_parameters,
-            "points": torch.stack(all_points),
-            "predictions": torch.stack(all_predictions),
+            "points": all_points,
+            "predictions": all_predictions,
         }
-    
-    # def infer(self, parameter_sets, output_pos=None, batch_size=1):
-    #     """
-    #     Perform inference for a set of parameters using the model.
-    #     Collect predictions together for easier analysis.
-    #     """
 
-    #     # Create a temporary dataset for inference
-    #     inference_dataset = ffsDataset(
-    #         root=self.base_dataset.root,
-    #         num_inputs=self.base_dataset.num_inputs,
-    #         num_outputs=self.base_dataset.num_outputs,
-    #         mode="inference",
-    #         crop_values=self.base_dataset.crop_values,
-    #         parameter_sets=parameter_sets,
-    #         useMesh=self.base_dataset.useMesh,
-    #         meshParameters=self.base_dataset.meshParameters,
-    #     )
+# class ffsInference:
+#     def __init__(self, base_dataset, numSupernodes,
+#                   model = None, device = None):
+        
+#         self.model = model
+#         self.base_dataset = base_dataset
+#         # self.totalPoints = totalPoints
+#         self.numSupernodes = numSupernodes
+#         self.device = device
+#         # self.xMin = base_dataset.crop_values[0][0]
+#         # self.xMax = base_dataset.crop_values[1][0]
+
+#         self.inference_dataset = copy.deepcopy(base_dataset)
 
 
-    #     # Create a DataLoader for inference
-    #     inference_dataloader = DataLoader(
-    #         dataset=inference_dataset,
-    #         batch_size=batch_size,
-    #         collate_fn=ffsCollator(num_supernodes=self.numSupernodes, deterministic=True),
-    #     )
+#     def inferOld(self, parameter_sets, output_pos=None, batch_size=1):
+#         """
+#         Perform inference using the same DataLoader setup as training.
+#         """
+#         # Create a temporary dataset for inference
+#         # self.inference_dataset = ffsDataset(
+#         #     root=self.base_dataset.root,
+#         #     num_inputs=self.base_dataset.num_inputs,
+#         #     num_outputs=self.base_dataset.num_outputs,
+#         #     mode="inference",
+#         #     crop_values=self.base_dataset.crop_values,
+#         #     parameter_sets=parameter_sets,
+#         #     useMesh=self.base_dataset.useMesh,
+#         #     meshParameters=self.base_dataset.meshParameters,
+#         #     customOutputPos=output_pos,
+#         # )
+        
+#         # self.inference_dataset = copy.deepcopy( self.base_dataset)
+#         self.inference_dataset.setInferenceMode(parameter_sets)
+#         self.inference_dataset.customOutputPos=output_pos
+        
+#         # Create a DataLoader for inference
+#         inference_dataloader = DataLoader(
+#             dataset=self.inference_dataset,
+#             batch_size=batch_size,
+#             collate_fn=ffsCollator(num_supernodes=self.numSupernodes, deterministic=True),
+#         )
 
-    #     all_predictions = []
-    #     all_parameters = []
-    #     all_points = []
+#         all_predictions = []
+#         all_parameters = []
+#         all_points = []
 
-    #     idx = 0
-    #     total_start_time = time.time()  # Start timing the entire inference process
+#         # Perform inference
+#         for batch in inference_dataloader:
+#             # for key, value in batch.items():
+#             #     if key == 'name':
+#             #         continue
+#             #     print(key, value.shape)
 
-    #     for re_value, Lo, Ho in parameter_sets:
-    #         step_start_time = time.time()  # Start timing this step
+#             with torch.no_grad():
+#                 y_hat = self.model(
+#                     input_feat=batch["input_feat"].to(self.device),
+#                     input_pos=batch["input_pos"].to(self.device),
+#                     supernode_idxs=batch["supernode_idxs"].to(self.device),
+#                     batch_idx=batch["batch_idx"].to(self.device),
+#                     output_pos=batch["output_pos"].to(self.device),
+#                     re=batch["re"].to(self.device),
+#                 )
+#             all_predictions.append(self.base_dataset.denormalize_feat(y_hat.cpu()))
+#             # all_parameters.extend(batch["name"])
+#             all_points.append(self.base_dataset.denormalize_pos(batch["output_pos"]))
 
-    #         # Preprocessing step
-    #         preprocess_start_time = time.time()
-    #         batch = self.preprocess(re_value, Lo, Ho, idx)
-    #         preprocess_end_time = time.time()
-    #         # print(f"Preprocessing for parameters (re={re_value}, Lo={Lo}, Ho={Ho}) took {preprocess_end_time - preprocess_start_time:.4f} seconds.")
 
-    #         # Output position setup
-    #         current_output_pos = output_pos if output_pos is not None else batch['output_pos']
-
-    #         # Inference step
-    #         inference_start_time = time.time()
-    #         with torch.no_grad():
-    #             y_hat = self.model(
-    #                 input_feat=batch['input_feat'].to(self.device),
-    #                 input_pos=batch['input_pos'].to(self.device),
-    #                 supernode_idxs=batch['supernode_idxs'].to(self.device),
-    #                 batch_idx=batch['batch_idx'].to(self.device),
-    #                 output_pos=current_output_pos.to(self.device),
-    #                 re=batch['re'].to(self.device),
-    #             )
-    #         inference_end_time = time.time()
-    #         # print(f"Inference for parameters (re={re_value}, Lo={Lo}, Ho={Ho}) took {inference_end_time - inference_start_time:.4f} seconds.")
-
-    #         # Postprocessing step
-    #         postprocess_start_time = time.time()
-    #         all_points.append(self.base_dataset.denormalize_pos(current_output_pos.squeeze()))
-    #         all_predictions.append(self.base_dataset.denormalize_feat(y_hat.cpu()))
-    #         all_parameters.append({'re': re_value, 'Lo': Lo, 'Ho': Ho})
-    #         postprocess_end_time = time.time()
-    #         # print(f"Postprocessing for parameters (re={re_value}, Lo={Lo}, Ho={Ho}) took {postprocess_end_time - postprocess_start_time:.4f} seconds.")
-
-    #         # Step timing
-    #         step_end_time = time.time()
-    #         # print(f"Total time for step {idx + 1} (re={re_value}, Lo={Lo}, Ho={Ho}) took {step_end_time - step_start_time:.4f} seconds.")
-
-    #         idx += 1
-
-    #     total_end_time = time.time()  # End timing the entire inference process
-    #     # print(f"Total inference process took {total_end_time - total_start_time:.4f} seconds.")
-
-    #     return {
-    #         'parameters': all_parameters,
-    #         'points': torch.stack(all_points),
-    #         'predictions': torch.stack(all_predictions)
-    #     }
+#         return {
+#             # "parameters": all_parameters,
+#             "points": torch.stack(all_points),
+#             "predictions": torch.stack(all_predictions),
+#         }
--- optimization.py
+++ optimization.py
@@ -13,11 +13,9 @@ import contextlib
 from multiprocessing import Process
 from pymoo.termination.default import DefaultMultiObjectiveTermination
 import torch
-
 from ffsInference import ffsInference
 
 class ffsOptProblem(Problem):
-
     def __init__(self, n_var, n_obj, xl, xu, inferencer: ffsInference, re=500.0, maxDesignsPerEvaluation=100):
         super().__init__(n_var=n_var, n_obj=n_obj, xl=xl, xu=xu)
         self.gen = 0
@@ -27,55 +25,110 @@ class ffsOptProblem(Problem):
 
     def _evaluate(self, allDesigns, out, *args, **kwargs):
         start_time = time.time()
-        # valuesF = []
-        # print(f'Generation {str(self.gen)}: Evaluating {str(allDesigns.shape[0])} Designs in {str(batches)} Batches')
-        # print("Generation " + str(self.gen) + ": Evaluating " + str(allDesigns.shape[0]) + " Designs in " + str(batches) + " Batches")
-        
-        # Create points to use for inference, one line upsteam and one line downstream of the step
+
+        # Create points to use for inference, one line upstream and one line downstream of the step
         upstreamX = -4
         downstreamX = 4
         numPoints = 100
         upstreamPoints = torch.tensor([[upstreamX, y] for y in np.linspace(-0.5, 0.5, numPoints)], dtype=torch.float32)
         downstreamPoints = torch.tensor([[downstreamX, y] for y in np.linspace(-0.5, 0.5, numPoints)], dtype=torch.float32)
-        points = torch.cat((upstreamPoints, downstreamPoints), dim=0) #.unsqueeze(0)
+        points = torch.cat((upstreamPoints, downstreamPoints), dim=0)
 
-        # points = self.inferencer.train_dataset.normalize_pos(points)
+        # Prepare parameter sets
+        re_column = np.full((allDesigns.shape[0], 1), self.re)
+        parameter_sets = np.concatenate((re_column, allDesigns), axis=1)
 
-        # fluidParameters
-        rho = 1.0
-        Um = 1.0
+        # Perform inference
+        results = self.inferencer.infer(parameter_sets=parameter_sets, output_pos=points, batch_size=self.maxDesignsPerEvaluation)
+        predictions = results['predictions']  # Shape: [num_designs * num_points, 3]
 
-        # for designs in np.array_split(ary=allDesigns, indices_or_sections=batches):
-        designs = allDesigns
-            # Inference
-            # Create a column of c values with the same number of rows as a
-        re_column = np.full((designs.shape[0], 1), self.re)
+        # Reshape predictions to [num_designs, num_points, 3]
+        num_designs = allDesigns.shape[0]
+        num_total_points = points.shape[0]
+        predictions = predictions.reshape(num_designs, num_total_points, -1)
 
-        # Concatenate along the second axis (columns)
-        parameter_sets = np.concatenate((re_column, designs), axis=1)
+        # Extract pressures
+        pressures = predictions[:, :, 2]  # Extract the pressure (p) component
 
-        # print("Parameter sets shape: ", parameter_sets.shape)
-        # print("Parameter sets: ", parameter_sets)
-        
-        results = self.inferencer.infer(parameter_sets=parameter_sets, output_pos=points)
-        prediction = results['predictions']
-        
-        upstreamPressures = prediction[:, :, 2][:,:numPoints]
-        downstreamPressures = prediction[:, :, 2][:,numPoints:]
+        # Split into upstream and downstream pressures
+        upstreamPressures = pressures[:, :numPoints]
+        downstreamPressures = pressures[:, numPoints:]
 
+        # Calculate average pressures
         upstreamPressure = upstreamPressures.mean(dim=1)
         downstreamPressure = downstreamPressures.mean(dim=1)
-        
-        dCp =  2*(upstreamPressure-downstreamPressure)/(rho*Um**2)
 
-        # print("dCp shape: ", dCp.shape)
-        # print("dCp: ", dCp)
+        # Calculate dCp
+        rho = 1.0
+        Um = 1.0
+        dCp = 2 * (upstreamPressure - downstreamPressure) / (rho * Um**2)
 
-        # valuesF.append(dCp.detach().cpu().numpy())
-
-        out["F"] = dCp.detach().cpu().numpy() #np.array(valuesF)
+        # Store results
+        out["F"] = dCp.detach().cpu().numpy()
         self.gen += 1
         elapsed_time = time.time() - start_time
-        print("Evaluation time: ", elapsed_time)
+        print(f"Evaluation time: {elapsed_time:.2f} seconds")
+
+# class ffsOptProblem(Problem):
+
+#     def __init__(self, n_var, n_obj, xl, xu, inferencer: ffsInference, re=500.0, maxDesignsPerEvaluation=100):
+#         super().__init__(n_var=n_var, n_obj=n_obj, xl=xl, xu=xu)
+#         self.gen = 0
+#         self.re = re
+#         self.maxDesignsPerEvaluation = maxDesignsPerEvaluation
+#         self.inferencer = inferencer
+
+#     def _evaluate(self, allDesigns, out, *args, **kwargs):
+#         start_time = time.time()
+#         # valuesF = []
+#         # print(f'Generation {str(self.gen)}: Evaluating {str(allDesigns.shape[0])} Designs in {str(batches)} Batches')
+#         # print("Generation " + str(self.gen) + ": Evaluating " + str(allDesigns.shape[0]) + " Designs in " + str(batches) + " Batches")
+        
+#         # Create points to use for inference, one line upsteam and one line downstream of the step
+#         upstreamX = -4
+#         downstreamX = 4
+#         numPoints = 100
+#         upstreamPoints = torch.tensor([[upstreamX, y] for y in np.linspace(-0.5, 0.5, numPoints)], dtype=torch.float32)
+#         downstreamPoints = torch.tensor([[downstreamX, y] for y in np.linspace(-0.5, 0.5, numPoints)], dtype=torch.float32)
+#         points = torch.cat((upstreamPoints, downstreamPoints), dim=0) #.unsqueeze(0)
+
+#         # points = self.inferencer.train_dataset.normalize_pos(points)
+
+#         # fluidParameters
+#         rho = 1.0
+#         Um = 1.0
+
+#         # for designs in np.array_split(ary=allDesigns, indices_or_sections=batches):
+#         designs = allDesigns
+#             # Inference
+#             # Create a column of c values with the same number of rows as a
+#         re_column = np.full((designs.shape[0], 1), self.re)
+
+#         # Concatenate along the second axis (columns)
+#         parameter_sets = np.concatenate((re_column, designs), axis=1)
+
+#         # print("Parameter sets shape: ", parameter_sets.shape)
+#         # print("Parameter sets: ", parameter_sets)
+        
+#         results = self.inferencer.infer(parameter_sets=parameter_sets, output_pos=points)
+#         prediction = results['predictions']
+        
+#         upstreamPressures = prediction[:, :, 2][:,:numPoints]
+#         downstreamPressures = prediction[:, :, 2][:,numPoints:]
+
+#         upstreamPressure = upstreamPressures.mean(dim=1)
+#         downstreamPressure = downstreamPressures.mean(dim=1)
+        
+#         dCp =  2*(upstreamPressure-downstreamPressure)/(rho*Um**2)
+
+#         # print("dCp shape: ", dCp.shape)
+#         # print("dCp: ", dCp)
+
+#         # valuesF.append(dCp.detach().cpu().numpy())
+
+#         out["F"] = dCp.detach().cpu().numpy() #np.array(valuesF)
+#         self.gen += 1
+#         elapsed_time = time.time() - start_time
+#         print("Evaluation time: ", elapsed_time)
 
 
